677d213714eee99547d62c0fcf927c558ebd19b3
Andy Kassen <atkassen@gmail.com>
2019-12-10 09:31:38 -0700

    Update cuda copy functions and device struct

--- include/cuda/copy.h
+++ include/cuda/copy.h
@@ -1,35 +1,41 @@
 #pragma once
 
 namespace cuda {
-	template <typename type>
-	void
-	dtoh(type* dst, const type* src, std::size_t count)
-	{
-		cudaMemcpy(dst, src, sizeof(type) * count,
-				cudaMemcpyDeviceToHost);
-	}
 
-	template <typename type>
-	void
-	dtod(type* dst, const type* src, std::size_t count)
-	{
-		cudaMemcpy(dst, src, sizeof(type) * count,
-				cudaMemcpyDeviceToDevice);
-	}
-
-	template <typename type>
-	void
-	htod(type* dst, const type* src, std::size_t count)
-	{
-		cudaMemcpy(dst, src, sizeof(type) * count,
-				cudaMemcpyHostToDevice);
-	}
-
-	template <typename type>
-	void
-	htoh(type* dst, const type* src, std::size_t count)
-	{
-		cudaMemcpy(dst, src, sizeof(type) * count,
-				cudaMemcpyHostToHost);
-	}
+template <typename type>
+void
+dtoh(type* dst, const type* src, std::size_t count)
+{
+	if (!count) return;
+	cudaMemcpy(dst, src, sizeof(type) * count,
+			cudaMemcpyDeviceToHost);
 }
+
+template <typename type>
+void
+dtod(type* dst, const type* src, std::size_t count)
+{
+	if (!count) return;
+	cudaMemcpy(dst, src, sizeof(type) * count,
+			cudaMemcpyDeviceToDevice);
+}
+
+template <typename type>
+void
+htod(type* dst, const type* src, std::size_t count)
+{
+	if (!count) return;
+	cudaMemcpy(dst, src, sizeof(type) * count,
+			cudaMemcpyHostToDevice);
+}
+
+template <typename type>
+void
+htoh(type* dst, const type* src, std::size_t count)
+{
+	if (!count) return;
+	cudaMemcpy(dst, src, sizeof(type) * count,
+			cudaMemcpyHostToHost);
+}
+
+} // namespace cuda
